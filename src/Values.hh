9393bb3b2e3a73e3f81eb9c625a51757fe875ca2
CharvN <Charvin@atlas.cz>
2016-10-20 00:48:18 +0200

    [#1] First skeleton of Value Representation interface+implementation

--- src/Values.hh
+++ src/Values.hh
@@ -3,10 +3,52 @@
 #include "Definitions.hh"
 #include "General.hh"
 
-class ValueRep {
-public:
-  ValueType type;
-  uint64_t value;
+#include <cstring>
 
-  /*ctr*/ ValueRep(const ValueType& type) : type{type}, value{} {};
+#include <boost/logic/tribool.hpp>
+
+/*
+ * Enum class representing Constraint Types - < > <= >= == !=
+ */
+enum class ConstraintType{
+	eLT = 0,
+	eGT,
+	eLTE,
+	eGTE,
+	eEQ,
+	eNEQ,
+};
+
+
+/*
+ * Interface of ValueRepresentation
+ */
+class IValueRep {
+public:
+
+	/*ctr*/ IValueRep(void) {};
+
+	virtual					~IValueRep(void) {};
+	virtual std::string		ToString() const = 0;
+
+	virtual boost::tribool	operator<(const IValueRep& other) const = 0;
+	virtual boost::tribool	operator>(const IValueRep& other) const = 0;
+	virtual boost::tribool	operator<=(const IValueRep& other) const = 0;
+	virtual boost::tribool	operator>=(const IValueRep& other) const = 0;
+	virtual boost::tribool	IsEQ(const IValueRep& other) const = 0;
+	virtual boost::tribool	IsNEQ(const IValueRep& other) const = 0;
+
+
+	virtual boost::tribool	operator<(const int64_t constValue) const = 0;
+	virtual boost::tribool	operator>(const int64_t constValue) const = 0;
+	virtual boost::tribool	operator<=(const int64_t constValue) const = 0;
+	virtual boost::tribool	operator>=(const int64_t constValue) const = 0;
+	virtual boost::tribool	IsEQ(const int64_t constValue) const = 0;
+	virtual boost::tribool	IsNEQ(const int64_t constValue) const = 0;
+	virtual boost::tribool	IsUnknown() const = 0;
+	virtual boost::tribool	IsZero() const = 0;
+
+	virtual void			SetConstraint( ConstraintType type, IValueRep &other) = 0;
+	virtual void			SetConstraint( ConstraintType type, int64_t constValue) = 0;
+
 };
