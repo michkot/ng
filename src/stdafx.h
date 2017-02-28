c23a429ed4294ead5e5728c3a2e55f66d233a6de
Michal Kotoun <xkotou04@stud.fit.vutbr.cz>
2017-02-28 19:37:54 +0000

    Addition on new MSVC project, should replace the old new; preparation for addition of license and restructuring of project

---
+++ src/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#if defined(_MSC_VER)
+
+#include "Definitions.hh"
+#include "Exceptions.hh"
+//#include <gsl/gsl_algorithm>
+#include <range/v3/all.hpp>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+#include <iostream>
+
+#endif
